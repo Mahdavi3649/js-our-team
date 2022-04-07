d7d65aa20b18837543ced7e070941e76163cff3d
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-04-07 15:46:04 +0200

    Completato  Bonus 1 & Bonus 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,8 +39,42 @@ const team = [
 
   for (let i = 0; i < team.length; i++) {
       const person = team[i];
-      console.log(person.name);
-      console.log(person.role);
-      console.log(person.image);
+      console.log(`${person.name}`);
+      console.log(`${person.role}`);
+      console.log(`${person.image}`);
       console.log("__________________");
   }
+
+ /*  MILESTONE 2 
+ (se ce la fai falla entro le 18 altrimenti puoi consegnare anche dopo le 18 - easy):
+stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1 
+(solo se non sei giá frustrato dalle milestone 1 e 2):
+trasformare la stringa foto in una immagine effettiva
+BONUS 2 
+(solo se non sei giá fuso):
+organizzare i singoli membri in card/schede */
+
+// selezionare l'elemento della dom dove voglio stampare le informazioni
+const personElement = document.querySelector(".row")
+    console.log(personElement)
+
+for (let i = 0; i < team.length; i++) {
+    const person = team[i];
+
+    const personCard = `
+    <div class="col">
+    <div class="card-top">
+        <img src="./assets/img/${person.image}">
+    </div>
+    <div class="card-text text-center m-4">
+        <h4>${person.name}</h4>
+        <h6>${person.role}</h6>
+    </div>
+</div>
+    `;
+    //console.log(personCard);
+
+    personElement.insertAdjacentHTML("beforeend", personCard);
+
+}
