3e5871984f807101725cd9d13c386a8f09dbbe93
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-04-07 14:47:21 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+
+
+/* MILESTONE 1 :
+stampare su console le informazioni di nome, ruolo e la stringa della foto */
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  console.log(team);
+
+  for (let i = 0; i < team.length; i++) {
+      const person = team[i];
+      console.log(person.name);
+      console.log(person.role);
+      console.log(person.image);
+      console.log("__________________");
+  }
